9104b88b18793a481fc8501dcc81eeab621e1c99
Callum Whyte <hey@callumwhyte.com>
2019-10-27 11:45:03 +0000

    Routing robots.txt to controller through domain route handler

---
+++ src/Our.Umbraco.DynamicRobots/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Our.Umbraco.DynamicRobots
+{
+    public class Constants
+    {
+        public const string RobotsRouteName = "Robots";
+
+        public const string RobotsRouteUrl = "robots.txt";
+    }
+}
---
+++ src/Our.Umbraco.DynamicRobots/Startup/RouteComponent.cs
@@ -0,0 +1,36 @@
+﻿using System.Web.Routing;
+using Our.Umbraco.DynamicRobots.Routing;
+using Umbraco.Core.Composing;
+using Umbraco.Web;
+
+namespace Our.Umbraco.DynamicRobots.Startup
+{
+    internal class RouteComponet : IComponent
+    {
+        private readonly IUmbracoContextFactory _umbracoContextFactory;
+
+        public RouteComponet(IUmbracoContextFactory umbracoContextFactory)
+        {
+            _umbracoContextFactory = umbracoContextFactory;
+        }
+
+        public void Initialize()
+        {
+            RouteTable.Routes.MapUmbracoRoute(
+                Constants.RobotsRouteName,
+                Constants.RobotsRouteUrl,
+                new
+                {
+                    controller = "Robots",
+                    action = "RenderRobots"
+                },
+                new RootNodeByDomainRouteHandler(_umbracoContextFactory)
+            );
+        }
+
+        public void Terminate()
+        {
+
+        }
+    }
+}
